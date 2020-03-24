15e9ca6333ce09122a9b0c8f1a83c31fe5cc66fb
JuissiJohtaja <juha.koivu@outlook.com>
2020-03-24 19:08:29 +0200

    Done

--- viikko6/QueryLanguage/src/main/java/statistics/QueryBuilder.java
+++ viikko6/QueryLanguage/src/main/java/statistics/QueryBuilder.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package statistics;
 
 import java.util.ArrayDeque;
@@ -14,10 +10,6 @@ import statistics.matcher.Matcher;
 import statistics.matcher.Or;
 import statistics.matcher.PlaysIn;
 
-/**
- *
- * @author juhakoivu
- */
 public class QueryBuilder {
     private ArrayDeque<Matcher> matchers;
 
--- viikko6/QueryLanguage/src/main/java/statistics/matcher/Or.java
+++ viikko6/QueryLanguage/src/main/java/statistics/matcher/Or.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package statistics.matcher;
 
 import statistics.Player;
 
-/**
- *
- * @author juhakoivu
- */
 public class Or implements Matcher  {
     private Matcher[] matchers;
 
